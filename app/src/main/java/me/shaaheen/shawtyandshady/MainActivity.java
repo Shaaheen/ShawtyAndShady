ab9c193764e197114a3872edd17d9c63b8b8ac3c
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-12-10 00:48:36 +0200

    Added a sendMessage method to be able to create a new entry in the online database with the message value and username

--- app/src/main/java/me/shaaheen/shawtyandshady/MainActivity.java
+++ app/src/main/java/me/shaaheen/shawtyandshady/MainActivity.java
@@ -6,39 +6,29 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    Firebase myFirebaseRef;
-
+    ChatManager chatManager;
     @Override
-         protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("BEFORE 1");
         Firebase.setAndroidContext(this);
+        chatManager = new ChatManager("https://boiling-torch-6214.firebaseio.com/Messages");
         System.out.println("BEFORE HTTP");
-        myFirebaseRef = new Firebase("https://boiling-torch-6214.firebaseio.com/");
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
-            }
-
-            @Override
-            public void onCancelled(FirebaseError error) {
-            }
-
-        });
-        System.out.println("BEFORE MESSAGE");
-        myFirebaseRef.child("message").setValue("Hi CARLAAAA AGAINNNN!!!!");
-        GoogleOAuthActivity ggle = new GoogleOAuthActivity();
-        ggle.starter();
     }
+
+    private void sendMessage(){
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
